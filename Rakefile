62927fb39aabb3dc6721f51dbe8ed67cec81ce99
harryuan65 <harryuan.65@gmail.com>
2022-02-09 12:41:06 +0800

    feat: update code generation template

--- Rakefile
+++ Rakefile
@@ -3,16 +3,11 @@ require 'erb'
 require_relative 'tools/solution_generator'
 
 desc 'Generate a file'
-task :solution, [:difficulty, :topic] do |_t, args|
-  unless args[:difficulty] && args[:topic]
+task :solution, [:url] do |_t, args|
+  unless args[:url]
     puts 'Missing args'
     exit
   end
-  unless %w[Easy Medium Hard].include?(args[:difficulty])
-    puts "Invalid difficulty: #{args[:difficulty]}"
-    exit
-  end
 
-  topic = args[:topic].gsub(/[.\s]/) { |match| match == ' ' ? '_' : '' }
-  SolutionGenerator.perform!(args[:difficulty], topic)
+  SolutionGenerator.perform!(args[:url])
 end
--- tools/solution_generator.rb
+++ tools/solution_generator.rb
@@ -1,18 +1,26 @@
 # frozen_string_literal: true
 
+require 'json'
 require_relative 'file_generator'
 require_relative 'folder_generator'
 
 # Generates a solution
 class SolutionGenerator
-  def self.perform!(difficulty, topic)
-    root = File.join(difficulty, topic)
+  def self.perform!(question_url)
+    topic = question_url[%r{https://leetcode.com/problems/(?<topic>[a-z0-9-]+)}, :topic]
+    raise 'Unprocessable url' unless topic
 
+    summary = JSON.parse(File.read('tools/question_summary.json'))[topic]
+    id, difficulty = summary.values_at('id', 'difficulty')
+    puts [id, difficulty, topic].join(' | ')
+    root = File.join(difficulty, "#{id}.#{topic}")
+    # https://leetcode.com/problems/remove-nth-node-from-end-of-list/
     FolderGenerator.perform!(root)
     FileGenerator.perform!(File.join(root, 'solution.rb'))
     FileGenerator.perform!(File.join(root, "#{topic}.md")) do |f|
+      @url = question_url
       template = File.read('tools/templates/solution.md.erb')
-      result = ERB.new(template).result
+      result = ERB.new(template).result(binding)
       f.write(result)
     end
     FolderGenerator.perform!(File.join(root, 'images'))
