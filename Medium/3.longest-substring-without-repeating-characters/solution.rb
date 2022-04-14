1f663d761fbe7991d5cd11ce092372a6db38fd3d
harryuan65 <harryuan.65@gmail.com>
2022-04-14 17:52:54 +0800

    feat: 3 lsw

---
+++ Medium/3.longest-substring-without-repeating-characters/solution.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'set'
+
+def solution(s = '')
+  m = 0
+  si = 0
+  set = Set.new
+  0.upto(s.size - 1) do |i|
+    if !set.include?(s[i])
+      set << s[i]
+    else
+      si += 1 while set.include?(si)
+    end
+
+    m = i - si if i - si > m
+  end
+  m
+end
--- Medium/3.longest-substring-without-repeating-characters/solution_spec.rb
+++ Medium/3.longest-substring-without-repeating-characters/solution_spec.rb
@@ -2,21 +2,29 @@ require_relative './solution'
 
 TEST_CASES = [
   {
-    input: [],
+    input: 'abcabcbb',
+    output: 3
+  },
+  {
+    input: '',
     output: 0
   },
   {
-    input: [],
-    output: 0
-  }
+    input: 'a',
+    output: 1
+  },
+  {
+    input: 'ababaasdertghda',
+    output: 7
+  },
 ].freeze
 
 RSpec.describe '#solution' do
   TEST_CASES.each do |tc|
     case tc
-    in { input: input, output: output}
+    in { input:, output: }
       it "#{input}  =>  #{output}" do
-        # expect(___).to eq(output)
+        expect(solution(input)).to eq(output)
       end
     end
   end
