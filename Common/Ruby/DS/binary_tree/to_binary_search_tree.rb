d662252d92f981e1504ec151d150f27356d34f35
harryuan65 <harryuan.65@gmail.com>
2022-07-29 14:01:36 +0800

    feat: add leetcode array to bst

---
+++ Common/Ruby/DS/binary_tree/to_binary_search_tree.rb
@@ -0,0 +1,31 @@
+require_relative "tree_node"
+
+class Array
+  def to_binary_search_tree
+    root_val, *rest = self
+    root = TreeNode.new(root_val)
+    queue = [root]
+    while queue.any?
+      # @type [TreeNode]
+      current_root = queue.shift
+      left_val, right_val = rest.shift(2)
+
+      if left_val
+        left = TreeNode.new(left_val)
+        current_root.left = left
+        queue << left
+      end
+
+      if right_val
+        right = TreeNode.new(right_val)
+        current_root.right = right
+        queue << right
+      end
+    end
+    root
+  end
+end
+
+# root = [6,2,8,0,4,7,9,nil,nil,3,5].to_binary_search_tree
+# require_relative "traversal"
+# pre_order(root)
