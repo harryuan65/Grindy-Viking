3278bc5626c3690a347474efbec67a90473e6cea
harryuan65 <harryuan.65@gmail.com>
2022-12-13 14:47:24 +0800

    feat: update fib iter

--- Common/Ruby/ALGO/fib/fib_iterative.rb
+++ Common/Ruby/ALGO/fib/fib_iterative.rb
@@ -1,9 +1,10 @@
 def fib2(n)
-  arr = [0, 1, 2]
-  return arr[n] if n < 2
+  return n if n < 2
 
+  arr = [0, 1, 2]
   (3..n).each do |i|
-    arr[i] = arr[i - 1] + arr[i - 2]
+    arr.push(arr[i - 1] + arr[i - 2])
   end
+
   arr[n]
 end
