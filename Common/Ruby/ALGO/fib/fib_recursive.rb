915214d67b3a6a7919ae8bd62358f6623a21d67b
harryuan65 <harryuan.65@gmail.com>
2022-12-13 14:47:38 +0800

    chore: update fib recursive comment

--- Common/Ruby/ALGO/fib/fib_recursive.rb
+++ Common/Ruby/ALGO/fib/fib_recursive.rb
@@ -6,9 +6,9 @@ def fib(n)
   end
 end
 
-# fib(5)
-# -> fib(4)                   +           fib(3)
-# -> fib(3)          + fib(2)            fib(2)  +  fib(1)
-# -> fib(2) + fib(1)  + fib(2)
-# ->  2 +     1     +    2              2    + 1 = 8
-# Base case
+#                                 fib(5)
+# ->             fib(4)              +              fib(3)
+# ->        fib(3)          + fib(2)            fib(2)  +  fib(1)
+# -> fib(2) + fib(1)
+# ->  2     +   1           +    2   +            2    +    1    =  8
+
